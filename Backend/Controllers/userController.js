a67e7a08d771cf33a80cf8f16338a4ec7f456725
Madhu <nmadhu1995@gmail.com>
2024-12-16 18:14:27 +0800

    bcrypt to bcryptjs

--- Backend/Controllers/userController.js
+++ Backend/Controllers/userController.js
@@ -1,81 +1,99 @@
-import userSchemaModal from "../Models/userModel.js"
-import { z } from "zod"
-import bcrypt from "bcrypt"
-import { generateTokenAndSaveInCookies } from "../Utils/token.js"
+import userSchemaModal from "../Models/userModel.js";
+import { z } from "zod";
+import bcryptjs from "bcryptjs";
+import { generateTokenAndSaveInCookies } from "../Utils/token.js";
 
-const userSchemaValidation = z.object(
-    {
-        email : z
-            .string()
-            .email({message: "Invalid email address"}),
-        username : z
-                  .string()
-                  .min(3, {message: "Username atleast 3 characters long"}),
-        password : z
-                  .string()
-                  .min(6, {message: "Password atleast 6 characters long"})
-    }
-)
+const userSchemaValidation = z.object({
+    email: z.string().email({ message: "Invalid email address" }),
+    username: z
+        .string()
+        .min(3, { message: "Username atleast 3 characters long" }),
+    password: z
+        .string()
+        .min(6, { message: "Password atleast 6 characters long" }),
+});
 
-
-export const signup = async(req,res) => {
+export const signup = async (req, res) => {
     try {
-        const {username, email, password} = req.body
+        const { username, email, password } = req.body;
 
-        const validate = userSchemaValidation.safeParse({username, email, password})
-        
-        if(!validate.success)
-        { 
-            const errorMessage = validate.error.errors.map((error)=>error.message)
-            return res.status(400).json({errors: errorMessage})
+        const validate = userSchemaValidation.safeParse({
+            username,
+            email,
+            password,
+        });
+
+        if (!validate.success) {
+            const errorMessage = validate.error.errors.map(
+                (error) => error.message
+            );
+            return res.status(400).json({ errors: errorMessage });
         }
 
-        const user = await userSchemaModal.findOne({email})
-        if(user)
-        { return res.status(400).json({message: "User already registered"}) }
-
-        const hashPassword = await bcrypt.hash(password,10)
-        const newUser = new userSchemaModal({username, email, password:hashPassword})
-        await newUser.save()
-        if(newUser)
-        { 
-            const token = await generateTokenAndSaveInCookies(newUser._id, res)
-            res.status(201).json({message:"User registered successfully", newUser, token:token}) 
+        const user = await userSchemaModal.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already registered" });
         }
 
+        const hashPassword = await bcryptjs.hash(password, 10);
+        const newUser = new userSchemaModal({
+            username,
+            email,
+            password: hashPassword,
+        });
+        await newUser.save();
+        if (newUser) {
+            const token = await generateTokenAndSaveInCookies(newUser._id, res);
+            res.status(201).json({
+                message: "User registered successfully",
+                newUser,
+                token: token,
+            });
+        }
     } catch (error) {
-        console.log(error)
-        return res.status(500).json({message: "Error registering user"})
+        console.log(error);
+        return res.status(500).json({ message: "Error registering user" });
     }
-}
+};
 
-export const login = async(req,res) => {
-    const {email,password} = req.body
+export const login = async (req, res) => {
+    const { email, password } = req.body;
     try {
-        if(!email || !password)
-        {return res.status(400).json({message: "All fields are required"})}
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
 
-        const user = await userSchemaModal.findOne({email}).select("+password")
-        if(!user || !(await bcrypt.compare(password , user.password)))
-        {return res.status(400).json({message: "Incorrect username or password"})}
+        const user = await userSchemaModal
+            .findOne({ email })
+            .select("+password");
+        if (!user || !(await bcryptjs.compare(password, user.password))) {
+            return res
+                .status(400)
+                .json({ message: "Incorrect username or password" });
+        }
 
-        const token = await generateTokenAndSaveInCookies(user._id, res)
-        res.status(200).json({message: "user logged in successfully", user, token})
-    
+        const token = await generateTokenAndSaveInCookies(user._id, res);
+        res.status(200).json({
+            message: "user logged in successfully",
+            user,
+            token,
+        });
     } catch (error) {
-        console.log(error)
-        return res.status(400).json({message: "Error in logging the user"})
+        console.log(error);
+        return res.status(400).json({ message: "Error in logging the user" });
     }
-}
+};
 
-export const logout = (req,res) => {
+export const logout = (req, res) => {
     try {
         res.clearCookie("jwt", {
-            path : "/"
-        })
-        res.status(200).json({message: "user logged out successfully"})
+            path: "/",
+        });
+        res.status(200).json({ message: "user logged out successfully" });
     } catch (error) {
-        console.log(error)
-        return res.status(400).json({message: "Error in logging out the user"})
+        console.log(error);
+        return res
+            .status(400)
+            .json({ message: "Error in logging out the user" });
     }
-}
+};
