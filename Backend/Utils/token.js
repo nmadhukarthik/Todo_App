c2711107d5d2f27d104a0aeecd9a80eb69fb67bd
Madhu <nmadhu1995@gmail.com>
2024-12-16 19:30:03 +0800

    token code change

--- Backend/Utils/token.js
+++ Backend/Utils/token.js
@@ -1,19 +1,25 @@
-import jwt from "jsonwebtoken"
-import userSchemaModal from "../Models/userModel.js"
+import jwt from "jsonwebtoken";
+import userSchemaModal from "../Models/userModel.js";
 
-export const generateTokenAndSaveInCookies = async(userId,res) => {
-   const token =jwt.sign({userId}, process.env.JWT_SECRET_KEY, {
-        expiresIn : "10d"
-        // expiresIn : new Date(Date.now() + 10 * 24 * 60 * 60 * 1000)  //10days expiry date
-   })
+export const generateTokenAndSaveInCookies = async (userId, res) => {
+    try {
+        const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "10d",
+            // expiresIn : new Date(Date.now() + 10 * 24 * 60 * 60 * 1000)  //10days expiry date
+        });
 
-   res.cookie("jwt", token, {
-        httpOnly : true,
-        secure : false,
-        sameSite : "lax",
-        path : "/"
-   })
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            //    secure : false,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            path: "/",
+        });
 
-   await userSchemaModal.findByIdAndUpdate(userId,{token})
-   return token
-}
+        await userSchemaModal.findByIdAndUpdate(userId, { token });
+        return token;
+    } catch (error) {
+        console.error("Error generating token:", error);
+        throw new Error("Token generation failed");
+    }
+};
